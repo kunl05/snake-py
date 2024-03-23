312164b3aea6a05f59ee9a0d12775523bbf13ed9
kunal <kkr783871@gmail.com>
2024-03-23 17:18:58 +0530

    22-03-2024

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import pygame
 from pygame.locals import *
+import time
+
 
 class Snake:
-    def __init__(self, parent_screen):
-        self.parent_screen = parent_screen
+    def __init__(self, surface):
+        self.parent_screen = surface
         self.block = pygame.image.load("pic/bl.png").convert()
         self.x = 100
         self.y = 100
+        self.direction = "down"
 
     def draw(self):
-        self.parent_screen.fill((95,94,5))
+        self.parent_screen.fill((238,168,73))
         self.parent_screen.blit(self.block,(self.x,self.y))
         pygame.display.flip()
 
     def move_up(self):
-        self.y -= 10
-        self.draw()
+        self.direction = "up"
     
     def move_down(self):
-        self.y += 10
-        self.draw()
+        self.direction = "down"
     
     def move_left(self):
-        self.x -= 10
-        self.draw()
+        self.direction = "left"
     
     def move_right(self):
-        self.x += 10
+        self.direction = "right"
+
+    
+
+
+    def walk(self):
+        if self.direction == "up":
+            self.y -=10
+        if self.direction == "down":
+            self.y +=10
+        if self.direction == "left":
+            self.x -=10
+        if self.direction == "right":
+            self.x +=10
+
         self.draw()
+
         
 
 
@@ -63,6 +78,10 @@ class Game:
 
                 elif event.type == QUIT:
                     running = False
+
+
+            self.snake.walk()
+            time.sleep(0.2)
 
         
 
